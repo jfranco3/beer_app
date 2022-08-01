1c6d5243dd2c2044cdc2305041b313a2f74e5ae3
Jenn Franco <jfranco376@gmail.com>
2022-08-01 13:08:24 -0500

    edit css

--- src/App.js
+++ src/App.js
@@ -4,16 +4,29 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      arrayOfBeer: [],
 };
+}
+
+componentDidMount() {
+  fetch('https://api.punkapi.com/v2/beers')
+   .then(response => {
+    return response.json()
+   }).then(arrayOfBeer => this.setState({arrayOfBeer}))
 }
 
  render() {
   return (
     <div>
-      <h2>YOur Component Works</h2>
+      <h2>Your Component Works</h2>
+      <ol>
+        {this.state.arrayOfBeer.map((beer, index) => {
+        return <li key={index}>
+          <h3>{beer.name}</h3>{beer.brewers_tips}</li>;
+      })}
+      </ol>
     </div>
-  )
+  );
  }
 }
 
