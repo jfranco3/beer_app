bc9666afba26e982c33b9c9312f669665fffc291
Jenn Franco <jfranco376@gmail.com>
2022-08-01 15:32:25 -0500

    edits

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       arrayOfBeer: [],
-};
-}
+    };
+  }
 
-componentDidMount() {
-  fetch('https://api.punkapi.com/v2/beers')
-   .then(response => {
-    return response.json()
-   }).then(arrayOfBeer => this.setState({arrayOfBeer}))
-}
+  componentDidMount() {
+    fetch("https://api.punkapi.com/v2/beers")
+      .then((response) => {
+        return response.json();
+      })
+      .then((arrayOfBeer) => this.setState({ arrayOfBeer }));
+  }
 
- render() {
-  return (
-    <div>
-      <h2>Your Component Works</h2>
-      <ol>
-        {this.state.arrayOfBeer.map((beer, index) => {
-        return <li key={index}>
-          <h3>{beer.name}</h3>{beer.brewers_tips}{beer.food_pairing}</li>;
-      })}
-      </ol>
-    </div>
-  );
- }
+  render() {
+    return (
+      <div>
+        <h2>Your Component Works</h2>
+        <ol>
+          {this.state.arrayOfBeer.map((beer, index) => {
+            return (
+              <li key={index}>
+                <h3>{beer.name}</h3>
+                <strong>Tips: </strong>{beer.brewers_tips}
+                <p><strong>Food Pairing: </strong>{beer.food_pairing}</p>
+              </li>
+            );
+          })}
+        </ol>
+      </div>
+    );
+  }
 }
 
 export default App;
